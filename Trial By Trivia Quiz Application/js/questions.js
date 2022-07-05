7703d8657605da15081cbbdfea5d3ff3fc2ff1ac
Rathod-Nirmish <107026738+Rathod-Nirmish@users.noreply.github.com>
2022-07-05 22:58:19 +0530

    Add files via upload

---
+++ Trial By Trivia Quiz Application/js/questions.js
@@ -0,0 +1,114 @@
+  // creating an array and passing the number, questions, options, and answers
+  let questions = [
+      {
+      numb: 1,
+      question: "What does “www” stand for in a website browser?",
+      answer: "world wide web",
+      options: [
+        "world wave web",
+        "worse web world",
+        "world wide web",
+        "world war wait",
+      ]
+    },
+      {
+      numb: 2,
+      question: "How long is an Olympic swimming pool in meters?",
+      answer: "50 meter",
+      options: [
+        "20 meter",
+        "30 meter",
+        "50 meter",
+        "60 meter"
+      ]
+    },
+      {
+      numb: 3,
+      question: "Which day is celebrated as World Environment Day?",
+      answer: "5 june",
+      options: [
+        "16 may",
+        "5 june",
+        "28 feb",
+        "1 june"
+      ]
+    },
+      {
+      numb: 4,
+      question: "Who among the following were the first native Indian rulers to issue their own coins?",
+      answer: "Saatavahanas",
+      options: [
+        " Pallavas",
+        "Mauryas",
+        "Saatavahanas",
+        "Cholas"
+      ]
+    },
+      {
+      numb: 5,
+      question: "Which among the following is the official language of Union Territory of Lakshadweep ?",
+      answer: " Malayalam",
+      options: [
+        "Tamil",
+        " Malayalam",
+        "Sinhala",
+        "Great Andamanese"
+      ]
+    },
+    
+      {
+       numb: 6,
+       question: "Elephanta Caves are predominantly attributed to which Hindu God?",
+       answer: "Lord Shiva",
+       options: [
+         "Lord Brahma",
+         "Lord Indra",
+         "Lord Shiva",
+         "Lord Hanuman"
+       ]
+     },
+     {
+      numb: 7,
+      question: "Which among the following are constituents of Brass?",
+      answer: "Zinc and Copper",
+      options: [
+        "Zinc and Copper",
+        "Iron and Zinc",
+        "Copper and Nickel",
+        "Iron and Coppe"
+      ]
+    },
+    {
+      numb: 8,
+      question: "Which country has issued its first passport with an “X” gender designation?",
+      answer: "USA",
+      options: [
+        "Australia",
+        "USA",
+        "Germany",
+        "India"
+      ]
+    },
+    {
+      numb: 9,
+      question: " which is the capital of Uttarakhad",
+      answer: "Dehradun",
+      options: [
+        "lucknow",
+        "prayagraj",
+        "Dehradun",
+        "shimla"
+      ]
+    },
+    {
+      numb: 10,
+      question: "Who is the first president of india",
+      answer: "Rajendra Prashad",
+      options: [
+        "Rajendra Prashad",
+        "Mahatma Gandhi",
+        "Sardar Vallabhbhai Patel",
+        "Jawaharlal Nehruji"
+      ]
+    },
+  ];
